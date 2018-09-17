9d408c4589d3a3a2e65351dfeb1688a5ecfb77e0
MarcinLukaszuk <m.lukaszuk427@gmail.com>
2018-09-17 18:10:01 +0200

    Add project files.

---
+++ ExamGenerator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamGenerator.Startup))]
+namespace ExamGenerator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
